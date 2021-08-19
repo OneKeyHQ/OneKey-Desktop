56c24143c3697f1cc2a4fc2399f60ca893d194c2
kwoktung <onlyif2009@gmail.com>
2021-08-19 20:06:44 +0800

    Update

--- packages/suite-desktop/pages/explore/index.tsx
+++ packages/suite-desktop/pages/explore/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { FC, ReactElement, useCallback, useEffect, useMemo, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -108,20 +108,41 @@ const CloseButton = styled.div`
 const ExploreContainer: FC<Props> = props => {
     const [tabs, setTabs] = useState<any[]>([]);
     const [activeTab, setActiveTab] = useState('home');
+
     const openTab = useCallback((dapp: any) => {
-        setTabs(p => {
-            setActiveTab(`StyledTabPane-${p.length}`);
-            return [...p, dapp];
-        });
-    }, []);
-    const closeTab = useCallback(index => {
-        setTabs(p => {
-            const newTabs = [...p];
-            newTabs.splice(index, 1);
-            return newTabs;
-        });
-    }, []);
+        const filtered = tabs.filter((tab: any) => tab.code === dapp.code)
+        if (filtered.length > 0) {
+            setActiveTab(dapp.code)
+            return
+        } else {
+            setActiveTab(dapp.code);
+            setTabs([...tabs, dapp])
+            return 
+        }
+    }, [tabs, setActiveTab, setTabs]);
+
+    const closeTab = useCallback((dapp: any) => {
+        console.log("tabs", tabs, "activeTab", activeTab, "dapp code", dapp.code)
+        const tabIndex = tabs.findIndex((tab: any) => tab.code === dapp.code)
+        let nextActiveTab = 'home'
+        if (tabIndex >= 1) {
+            nextActiveTab = tabs[tabIndex - 1].code
+        }
+        if (activeTab === dapp.code) {
+            console.log("nextActiveTab", nextActiveTab)
+            setActiveTab(() => nextActiveTab);
+        }
+        const newTabs = tabs.filter((tab: any) => tab.code !== dapp.code)
+        console.log("newTabs", newTabs, "nextActiveTab", nextActiveTab)
+        setTabs(() => newTabs)
+    }, [tabs, setTabs, activeTab, setActiveTab]);
+
+    useEffect(() => {
+        console.log("activeTab", activeTab, "tabs", tabs)
+    }, [activeTab, tabs])
+
     const updateTabs = (index: number) => (_tab: any) => {
+        console.log("==== updateTabs =====", _tab)
         setTabs(p => {
             const tab = typeof _tab === 'function' ? _tab(p[index]) : _tab;
             const newTabs = [...p];
@@ -129,46 +150,45 @@ const ExploreContainer: FC<Props> = props => {
             return newTabs;
         });
     };
-    useEffect(() => {
-        const parts = activeTab.split('-');
-        const index = parts[parts.length - 1];
-        if (tabs.length <= parseInt(index, 10)) {
-            setActiveTab('home');
+
+    let renderTabBar: RenderTabBar = useMemo(() => {
+        return props => {
+            const tabsMap = tabs.reduce((res, item) => {
+                res[item.code] = item;
+                return res
+            }, {})
+            return (
+                <StyledTabBar show={tabs.length > 0}>
+                    {props.panes.flat(1).map((node: ReactElement) => {
+                        const dapp = tabsMap[node.key as string];
+                        return (
+                            <StyledTabNode
+                                key={node.key}
+                                active={activeTab === node.key}
+                                onClick={() => props.onTabClick(node.key)}
+                            >
+                                {dapp?.favicon && dapp?.favicon.startsWith('http') && (
+                                    <Favicon src={dapp?.favicon} />
+                                )}
+                                <TabTitle>{dapp?.title ?? dapp?.name ?? node.props.tab}</TabTitle>
+                                {node.key !== 'home' && <CloseButton onClick={() => closeTab(dapp)} />}
+                            </StyledTabNode>
+                        );
+                    })}
+                </StyledTabBar>
+            );
         }
-    }, [activeTab, tabs]);
-    const renderTabBar: RenderTabBar = props => {
-        return (
-            <StyledTabBar show={tabs.length > 0}>
-                {props.panes.flat(1).map((node: ReactElement) => {
-                    const parts = (node.key as string).split('-');
-                    const index = parseInt(parts[parts.length - 1], 10);
-                    const dapp = tabs[index];
-                    return (
-                        <StyledTabNode
-                            key={node.key}
-                            active={activeTab === node.key}
-                            onClick={() => props.onTabClick(node.key)}
-                        >
-                            {dapp?.favicon && dapp?.favicon.startsWith('http') && (
-                                <Favicon src={dapp?.favicon} />
-                            )}
-                            <TabTitle>{dapp?.title ?? dapp?.name ?? node.props.tab}</TabTitle>
-                            {node.key !== 'home' && <CloseButton onClick={() => closeTab(index)} />}
-                        </StyledTabNode>
-                    );
-                })}
-            </StyledTabBar>
-        );
-    };
-    const body = (
-        <StyledTabs activeKey={activeTab} onChange={setActiveTab} renderTabBar={renderTabBar}>
+    }, [tabs, activeTab, closeTab]);
+
+    const body = useMemo(() => {
+        return <StyledTabs activeKey={activeTab} onChange={setActiveTab} renderTabBar={renderTabBar}>
             <StyledTabPane tab="home" key="home">
                 <Container {...props} openTab={openTab} setDapp={() => null} />
             </StyledTabPane>
             {tabs.map((dapp, index) => (
-                <StyledTabPane tab={dapp.name} key={`StyledTabPane-${index}`} forceRender>
+                <StyledTabPane tab={dapp.name} key={dapp.code} forceRender>
                     <Container
-                        key={`Container-${index}`}
+                        key={dapp.code}
                         {...props}
                         dapp={dapp}
                         openTab={openTab}
@@ -177,7 +197,7 @@ const ExploreContainer: FC<Props> = props => {
                 </StyledTabPane>
             ))}
         </StyledTabs>
-    );
+    },[tabs, openTab, updateTabs, activeTab, setActiveTab, renderTabBar]);
     return <Explore key="explore" menu={body} />;
 };
 
