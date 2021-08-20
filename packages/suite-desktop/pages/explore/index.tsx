fb9248653fe41a1a9cc9403e7e61db9cae3e9254
kwoktung <onlyif2009@gmail.com>
2021-08-20 18:00:33 +0800

    fix dapp discovery tab display mistake

--- packages/suite-desktop/pages/explore/index.tsx
+++ packages/suite-desktop/pages/explore/index.tsx
@@ -105,80 +105,103 @@ const CloseButton = styled.div`
     }
 `;
 
-const ExploreContainer: FC<Props> = props => {
-    const [tabs, setTabs] = useState<any[]>([]);
-    const [activeTab, setActiveTab] = useState('home');
-    const openTab = useCallback((dapp: any) => {
-        setTabs(p => {
-            setActiveTab(`StyledTabPane-${p.length}`);
-            return [...p, dapp];
-        });
-    }, []);
-    const closeTab = useCallback(index => {
-        setTabs(p => {
-            const newTabs = [...p];
-            newTabs.splice(index, 1);
-            return newTabs;
-        });
-    }, []);
-    const updateTabs = (index: number) => (_tab: any) => {
-        setTabs(p => {
-            const tab = typeof _tab === 'function' ? _tab(p[index]) : _tab;
-            const newTabs = [...p];
-            newTabs.splice(index, 1, tab);
-            return newTabs;
-        });
-    };
-    useEffect(() => {
-        const parts = activeTab.split('-');
-        const index = parts[parts.length - 1];
-        if (tabs.length <= parseInt(index, 10)) {
-            setActiveTab('home');
+class ExploreContainer extends React.Component<Props, { tabs: any[], activeTab: string }> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            tabs: [],
+            activeTab: 'home'
         }
-    }, [activeTab, tabs]);
-    const renderTabBar: RenderTabBar = props => {
-        return (
-            <StyledTabBar show={tabs.length > 0}>
-                {props.panes.flat(1).map((node: ReactElement) => {
-                    const parts = (node.key as string).split('-');
-                    const index = parseInt(parts[parts.length - 1], 10);
-                    const dapp = tabs[index];
-                    return (
-                        <StyledTabNode
-                            key={node.key}
-                            active={activeTab === node.key}
-                            onClick={() => props.onTabClick(node.key)}
-                        >
-                            {dapp?.favicon && dapp?.favicon.startsWith('http') && (
-                                <Favicon src={dapp?.favicon} />
-                            )}
-                            <TabTitle>{dapp?.title ?? dapp?.name ?? node.props.tab}</TabTitle>
-                            {node.key !== 'home' && <CloseButton onClick={() => closeTab(index)} />}
-                        </StyledTabNode>
-                    );
-                })}
-            </StyledTabBar>
-        );
-    };
-    const body = (
-        <StyledTabs activeKey={activeTab} onChange={setActiveTab} renderTabBar={renderTabBar}>
-            <StyledTabPane tab="home" key="home">
-                <Container {...props} openTab={openTab} setDapp={() => null} />
-            </StyledTabPane>
-            {tabs.map((dapp, index) => (
-                <StyledTabPane tab={dapp.name} key={`StyledTabPane-${index}`} forceRender>
-                    <Container
-                        key={`Container-${index}`}
-                        {...props}
-                        dapp={dapp}
-                        openTab={openTab}
-                        setDapp={updateTabs(index)}
-                    />
+    }
+
+    openTab = (dapp: any) => {
+        const { tabs } = this.state
+        const filtered = tabs.filter((tab: any) => tab.code === dapp.code);
+        if (filtered.length > 0) {
+            this.setState({ activeTab: dapp.code })
+        } else {
+            this.setState({
+                tabs: ([] as any[]).concat(tabs, dapp),
+                activeTab: dapp.code
+            })
+        }
+    }
+
+    closeTab = (dapp: any) => {
+        const { tabs, activeTab } = this.state
+        if (activeTab === dapp.code) {
+            const tabIndex = tabs.findIndex((tab: any) => tab.code === dapp.code)
+            let nextActiveTab = 'home'
+            if (tabIndex >= 1) {
+                nextActiveTab = tabs[tabIndex - 1].code
+            }
+            this.setState({ activeTab : nextActiveTab })
+        }
+        const newTabs = tabs.filter((tab: any) => tab.code !== dapp.code)
+        this.setState({ tabs: newTabs })
+        
+    }
+
+    updateTabs = (index: number) => {
+        return (_tab: any) => {
+            const { tabs } = this.state
+            const tab = typeof _tab === 'function' ? _tab(tabs[index]) : _tab;
+            const newTabs = [...tabs];
+            newTabs.splice(index, 1, tab);
+            this.setState({ tabs: newTabs })
+        }
+    }
+
+    render() {
+        const { tabs, activeTab } = this.state
+        const tabsMap = tabs.reduce((res, item) => {
+            res[item.code] = item
+            return res
+        }, {})
+        const renderTabBar: RenderTabBar = props => {
+            return (
+                <StyledTabBar show={tabs.length > 0}>
+                    {props.panes.flat(1).map((node: ReactElement) => {
+                        
+                        const dapp = tabsMap[node.key as string];
+                        return (
+                            <StyledTabNode
+                                key={node.key}
+                                active={activeTab === node.key}
+                                onClick={() => props.onTabClick(node.key)}
+                            >
+                                {dapp?.favicon && dapp?.favicon.startsWith('http') && (
+                                    <Favicon src={dapp?.favicon} />
+                                )}
+                                <TabTitle>{dapp?.title ?? dapp?.name ?? node.props.tab}</TabTitle>
+                                {node.key !== 'home' && <CloseButton onClick={(e) => { e.stopPropagation(); this.closeTab(dapp) }} />}
+                            </StyledTabNode>
+                        );
+                    })}
+                </StyledTabBar>
+            );
+        };
+        const body = (
+            <StyledTabs activeKey={activeTab} onChange={(activeTab) => this.setState({ activeTab })} renderTabBar={renderTabBar}>
+                <StyledTabPane tab="home" key="home">
+                    <Container {...this.props} openTab={this.openTab} setDapp={() => null} />
                 </StyledTabPane>
-            ))}
-        </StyledTabs>
-    );
-    return <Explore key="explore" menu={body} />;
-};
+                {tabs.map((dapp, index) => (
+                    <StyledTabPane tab={dapp.name} key={dapp.code} forceRender>
+                        <Container
+                            key={dapp.code}
+                            {...this.props}
+                            dapp={dapp}
+                            openTab={this.openTab}
+                            setDapp={this.updateTabs(index)}
+                        />
+                    </StyledTabPane>
+                ))}
+            </StyledTabs>
+        );
+    
+        return <Explore key="explore" menu={body} />;
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExploreContainer);
