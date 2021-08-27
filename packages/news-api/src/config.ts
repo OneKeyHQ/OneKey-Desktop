9cbbc06c1b12c511a3e0bb58abb453a0d1c4ba5d
zhao.zhang <loatheb.zhao@gmail.com>
2021-08-27 17:47:16 +0800

    fix: remove translation-manager & landing-page

--- packages/news-api/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const MEDIUM_FEED_URL = 'https://cdn.onekey.so/dynamic/medium/feed/onekey-security-blog';
-export const MEDIUM_CDN_BASE = 'https://cdn-images-1.medium.com';
-export const ONEKEY_CDN_BASE = 'https://cdn.onekey.so/static/medium/images';
