f53c637628dadfbd2cd60ffdf389a902f0b46b42
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-08 12:14:56 +0800

    Init swap cra

---
+++ packages/web-swap/README.md
@@ -0,0 +1,17 @@
+# @OneKey/web-swap
+
+## Development
+
+The repo is managed as a monorepo
+
+### Install Dependencies
+
+```bash
+yarn
+```
+
+### Run
+
+```bash
+yarn start
+```
---
+++ packages/web-swap/src/pages/App.tsx
@@ -0,0 +1,9 @@
+export const Home = (): JSX.Element => (
+    <div>
+        <div className="h-full w-full flex flex-col lg:flex-row">
+            <div className="flex flex-col w-full pt-4 items-center flex-1 z-1 sm:p-4 sm:pt-8" />
+        </div>
+    </div>
+);
+
+export default Home;
